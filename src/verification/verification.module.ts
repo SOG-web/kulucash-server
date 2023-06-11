056d3fee007d0af52a70695d232c1a2a2cba426d
SOG-web <raheemolalekanusman84@gmail.com>
2023-06-11 19:18:57 +0100

    finished the verification team endpoint

--- src/verification/verification.module.ts
+++ src/verification/verification.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { VerificationController } from './verification.controller';
 import { VerificationService } from './verification.service';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CommonService } from 'src/common/common.service';
 
 @Module({
   controllers: [VerificationController],
-  providers: [VerificationService]
+  providers: [VerificationService, PrismaService, CommonService],
 })
 export class VerificationModule {}
