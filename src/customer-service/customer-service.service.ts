c85e3a3ae749e40aadbd3f6372008f7d5f507c49
SOG-web <raheemolalekanusman84@gmail.com>
2023-06-11 20:25:01 +0100

    added the steps of customer service

--- src/customer-service/customer-service.service.ts
+++ src/customer-service/customer-service.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class CustomerServiceService {}
+export class CustomerServiceService {
+  // available chat list: push the customer that a waiting to be assigned to a customer service
+  // currently assigned chat: push the customer that is currently assigned to a customer service
+  // take note of the resolve button
+  // !STEPs
+  // ?1. when a staff login to the system, the system will check if there is any available chat on the chatlist from the database
+  // ?2. check if the staff is currently assigned to a chat on the chatlist from the database
+  // ?3. if not, the system will assign the chat to the staff
+  // ?4. alaways check when a user disconnect from the soket and remove the user from the chatlist and delete there message from the database
+}
