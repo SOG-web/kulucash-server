7562116ceadf48c5c1363bc9162d05739ec6c8cf
SOG-web <raheemolalekanusman84@gmail.com>
2023-06-17 07:48:21 +0100

    added loan overdue category updater function to the cron jobs

--- src/crons/crons.service.ts
+++ src/crons/crons.service.ts
@@ -1,9 +1,14 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { Cron, CronExpression } from '@nestjs/schedule';
+import { OverDueCategory } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { updateLoansOverdueCategory } from './jobs/jobs';
 
 @Injectable()
 export class CronsService {
   private readonly logger = new Logger(CronsService.name);
+
+  constructor(private prisma: PrismaService) {}
 
   @Cron(CronExpression.EVERY_30_SECONDS)
   handleCron() {
@@ -11,8 +16,11 @@ export class CronsService {
   }
 
   @Cron(CronExpression.EVERY_DAY_AT_MIDNIGHT)
-  everdayMidnightCron() {
+  async everdayMidnightCron() {
     this.logger.debug('Called every day at midnight');
-    //TODO: check loans that are due for payment and add due category
+    //DONE: check loans that are due for payment and add due category
+    updateLoansOverdueCategory(this.prisma, this.logger);
+    //TODO: check cleared loan intrest, if it is up to 24 hours and loan not cleared return the loan intrest to the loan
+    //TODO: check loans that are due for payment and send email to users
   }
 }
